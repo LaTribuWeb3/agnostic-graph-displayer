b056f56dbb0456af960d0f72503950cb1dc994ed
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-05-17 12:49:08 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import logo from './logo.svg';
+
+async function getGraphs(entity, repo, dir) {
+  // get files
+  const url = `https://api.github.com/repos/${entity}/${repo}/git/trees/main`;
+  const dirTreeResponse = await axios.get(url);
+  const dirSHA = dir ? dirTreeResponse.data.tree.find(_ => _.type ==='tree' && _.name === dir) : dirTreeResponse.data.sha;
+  const dirUrl = `https://api.github.com/repos/${entity}/${repo}/git/trees/${dirSHA}`;
+  const filesResponse = await axios.get(dirUrl);
+  const allImagesNames = filesResponse.data.tree.filter(_ => _.path.endsWith('.jpg')).map(_ => _.path);
+  console.log(allImagesNames);
+  return allImagesNames;
+}
+
+function button(){
+
+
+  return <div>
+    <div className='button-left'></div>
+  </div>
+}
+
 function App() {
+  const [data, setData] = useState([]);
+  // get all images
+  ///get parameters for call
+  const urlParams = new URLSearchParams(window.location.search);
+  const entity = urlParams.get('entity');
+  const repo = urlParams.get('repo');
+  const dir = urlParams.get('dir');
+
+
+  //// recursive call to get all images
+  useEffect(() => {
+    async function getData() {
+      const data = await getGraphs(entity, repo, dir);
+      setData(data);
+    }
+    getData();
+  }, []);
+
+  //buttons
+
   return (
     <div className="App">
       <header className="App-header">
